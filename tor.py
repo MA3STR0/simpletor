bc286ba665cf5143ade4ab0b1d30f1f002a505d5
Anton <git@maestr0.com>
2014-11-30 21:12:22 +0100

    Start __init__ in tor.py

--- tor.py
+++ tor.py
@@ -1,4 +1,9 @@
+import socks
+import socket
+
+
 class Tor(object):
     """Tor class for socks proxy and controller"""
-    def __init__(self):
-        pass
+    def __init__(self, socks_port=9050):
+        self.socks_port = socks_port
+        self.default_socket = socket.socket
