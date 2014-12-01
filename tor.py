4ca8b6140ea68ee3a4824220590ecd7150cf90a4
Anton <git@maestr0.com>
2014-12-01 17:44:35 +0100

    Add connect and disconnect methods

--- tor.py
+++ tor.py
@@ -7,3 +7,12 @@ class Tor(object):
     def __init__(self, socks_port=9050):
         self.socks_port = socks_port
         self.default_socket = socket.socket
+
+    def connect(self):
+        """connect to Tor socks proxy"""
+        socks.set_default_proxy(socks.SOCKS5, "127.0.0.1", self.socks_port)
+        socket.socket = socks.socksocket
+
+    def disconnect(self):
+        """disconnect Tor socks proxy"""
+        socket.socket = self.default_socket
